e948bb5980ced96bc593bd4eacca5403001d2f86
VulnX <62636727+VulnX@users.noreply.github.com>
2025-09-03 16:43:23 +0530

    fix: Direct addressing to client side routes (#16)

--- server/src/web/mod.rs
+++ server/src/web/mod.rs
@@ -5,20 +5,12 @@ use axum::{
     routing::get,
     Router,
 };
-use tower_http::cors;
 
 pub fn app() -> Router {
     let route_apis = api::routes();
     Router::new()
-        .route("/", get(serve_frontend))
         .nest("/api", route_apis)
-        .layer(
-            cors::CorsLayer::new()
-                .allow_origin(cors::Any)
-                .allow_methods(cors::Any)
-                .allow_headers(cors::Any)
-                .expose_headers(cors::Any),
-        )
+        .fallback(get(serve_frontend))
 }
 
 async fn serve_frontend() -> impl IntoResponse {
